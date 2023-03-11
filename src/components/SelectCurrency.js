f4073b6873d8578b2c1b5718d6044cfd7f7d8d54
Biswanath Das <cv.biswanath@gmail.com>
2023-03-11 16:52:25 +0530

    ok

--- src/components/SelectCurrency.js
+++ src/components/SelectCurrency.js
@@ -9,16 +9,34 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 
 import TwoThousand from "../assets/2000.jpg";
+import FiveHundrad from "../assets/500.jpg";
+import TwoHundrad from "../assets/200.jpg";
+import OneHundrad from "../assets/100.jpg";
 
 export default function SelectCurrency() {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={8}>
+        <Grid item xs={8} style={{ display: "none" }}>
           <Card sx={{ maxWidth: 345 }}>
             <CardMedia
               sx={{ height: 140 }}
               image={TwoThousand}
+              title="green iguana"
+            />
+            <CardMedia
+              sx={{ height: 140 }}
+              image={FiveHundrad}
+              title="green iguana"
+            />
+            <CardMedia
+              sx={{ height: 140 }}
+              image={TwoHundrad}
+              title="green iguana"
+            />
+            <CardMedia
+              sx={{ height: 140 }}
+              image={OneHundrad}
               title="green iguana"
             />
             <CardContent>2000</CardContent>
